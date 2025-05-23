3449651b5c07f886d88ddc6c7eb86c62a6303fc3
Minaj <minajinamdar9@gmail.com>
2025-05-24 01:30:05 +0530

    first section design

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,17 @@
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Home(){
 	return(
 		<>
-		   <div> Home page  </div>
+		   <div className="bg-[#000814] px-5 " > 
+		    <p className="text-[#999DAA] p-3 bg-[#161D29] inline-flex items-center rounded-full shadow-sm shadow-[#FFFFFF2E] " > Become an instructor <FaArrowRight className="ml-2"  />  </p> 
+		    <h1 className="text-white text-[30px] font-[600] " >Empower Your Future with 
+		    <span className= "text-transparent bg-clip-text bg-linear-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB]" >
+		    {" "} Coding Skills
+		    </span> </h1>
+		    <p className="text-[#838894] text-[16px] " > With our online coding courses, you can learn at your own pace, from anywhere in the world,
+		     and get access to a wealth of resources, including hands-on projects, quizzes, and personalized
+		     feedback instructor   </p>
+		   </div>
 		</>)
 }
